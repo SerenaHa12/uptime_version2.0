dd5230107ccd63f4ccb7f734f9553a1b34d67b46
SerenaHa12 <serenavander.ha@gmail.com>
2023-07-04 16:18:22 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Sidebar } from './components';
 import { Dashboard, Asset, Ticket, Report, New, Setting } from './pages';
